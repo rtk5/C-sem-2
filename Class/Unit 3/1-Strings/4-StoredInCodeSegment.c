eb2ca3720aded3f6c97900575d5c5e5fa68eafae
rtk5 <rithvik.matta@gmail.com>
2024-03-26 09:33:23 +0530

    mar 26

--- Class/Unit 3/1-Strings/4-StoredInCodeSegment.c
+++ Class/Unit 3/1-Strings/4-StoredInCodeSegment.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main() {
     char*p1="bits";
-    *(p1+1)='e';
-    printf("%s \n",*p1);
+    *(p1+1)='e';        //won't update
+    printf("%s \n",*p1);    
     
     return 0;
 }
