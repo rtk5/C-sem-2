f76945621b6762fe0d11f427e2dc5ebd501b15aa
rtk5 <rithvik.matta@gmail.com>
2024-04-25 11:57:20 +0530

    apr 25

---
+++ Class/Unit 3/4-Link list/1-LinkList.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node*link;
+};
+
+struct node *header=NULL;
+void addbeg(int);
+void display(void);
+
+int main() {
+    int x, opt;
+    while(1) {
+        printf("Enter the option (1. Add element to the beginning, 2. Display list, 3. Exit): ");
+        scanf("%d", &opt);
+        switch(opt) {
+            case 1: 
+                printf("Enter element to be inserted: ");
+                scanf("%d", &x);
+                addbeg(x);
+                break;
+            case 2:
+                printf("Linked List: ");
+                display();
+                break;
+            case 3:
+                exit(0); // Exit the program
+            default:
+                printf("Invalid option\n");
+        }
+    }
+    return 0;
+}
+
+void addbeg(int x) {
+    struct node *newnode;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    if(newnode == NULL) {
+        printf("Out of memory\n");
+        return;
+    }
+    else {
+        newnode->data = x;
+        newnode->link = header;
+        header = newnode;
+    }
+}
+
+
+void display() {
+    struct node *p=header;
+    while(p!=NULL) {
+        printf("%d\n",p->data);
+        p=p->link;
+    }
+}
